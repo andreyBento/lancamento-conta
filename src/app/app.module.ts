3da5a37dda8c2c45f101db0e26213fe7754a5782
andrey <andreybenny@gmail.com>
2022-10-08 15:46:38 -0300

    Font Awesome, Header e Breadcrumb components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BreadcrumbComponent} from "../components/breadcrumb/breadcrumb.component";
+import {HeaderComponent} from "../components/header/header.component";
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faUser, faMoneyBill, faWallet, faCalendar, faCreditCard, faList, faCheck, faTimes, faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        BreadcrumbComponent,
+        HeaderComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faUser, faMoneyBill, faWallet, faCalendar, faCreditCard, faList, faCheck, faTimes, faCaretLeft, faCaretRight);
+  }
+}
---
+++ src/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss']
+})
+export class BreadcrumbComponent {
+  title = 'breadcrumb';
+}
